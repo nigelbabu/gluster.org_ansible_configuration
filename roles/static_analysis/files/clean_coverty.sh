2f102b9eb85857f869adebba187d6264ac6fba6c
Michael Scherer <misc@redhat.com>
2017-02-21 18:38:25 +0100

    Fix script to run on 15 days rather than 45, and 10 for master

--- roles/static_analysis/files/clean_coverty.sh
+++ roles/static_analysis/files/clean_coverty.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd /var/www/html/pub/gluster/glusterfs/static-analysis/
-for i in release-* master;
+for i in release-* ;
 do
-    find $i -type f -mtime +45 -exec rm -f {} \; > /dev/null
+    find $i -type f -mtime +15 -exec rm -f {} \; > /dev/null
 done
+find master -type f -mtime +10 -exec rm -f {} \; > /dev/null
 find . -type d -empty -exec rmdir {} \; > /dev/null
