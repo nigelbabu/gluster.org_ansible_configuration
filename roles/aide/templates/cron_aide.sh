ac63c3c05e0f7b4cab555db29352250774cfa5d8
Michael Scherer <misc@redhat.com>
2017-03-02 18:59:25 +0100

    Redirect also aide output in the log file

--- roles/aide/templates/cron_aide.sh
+++ roles/aide/templates/cron_aide.sh
@@ -5,7 +5,7 @@ if [ -f /srv/aidedb.gz ]; then
     /sbin/aide -c /etc/aide_mirror.conf -i
     cp -f /srv/aidedb.new.gz /srv/aidedb.gz
 else
-    /sbin/aide -c /etc/aide_mirror.conf -u >> /var/log/aide_scan.log
+    /sbin/aide -c /etc/aide_mirror.conf -u >> /var/log/aide_scan.log 2>&1
     cp -f /srv/aidedb.new.gz /srv/aidedb.gz
 fi;
 (echo "End of the run " ; date -u) >> /var/log/aide_scan.log
