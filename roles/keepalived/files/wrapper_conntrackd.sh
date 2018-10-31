f7d9c8a0ee6b71f6b87305a9f240b332077ace19
Michael Scherer <misc@redhat.com>
2018-10-31 15:09:44 +0100

    Add the status in the motd

--- roles/keepalived/files/wrapper_conntrackd.sh
+++ roles/keepalived/files/wrapper_conntrackd.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sed -i '/^Keepalived status:.*$/d' /etc/motd
+echo "Keepalived status: $1" >> /etc/motd
+
 if [ -f /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh ] ; then
     bash /usr/share/doc/conntrack-tools/doc/sync/primary-backup.sh $*
 fi
